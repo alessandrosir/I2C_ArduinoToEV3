7f7499d8f08b1c6769c6b04185a742cf0b9fc32c
alessandro sir <alessandrosiir@gmail.com>
2024-02-05 20:50:09 -0300

    28/08/2023

--- I2C_ArduinoToEV3.ino
+++ I2C_ArduinoToEV3.ino
@@ -1,23 +1,14 @@
 #include <Wire.h>
 #include <Servo.h>
 
-#define SLAVE_ADDRESS 0x04  // endereço da comunicação I2C
+#define SLAVE_ADDRESS 0x04  // endereço utlizado na comunicação I2C
 
 class Ultrasonic {
 private:
-  int trigPin, echoPin;
+  int trigPin, echoPin;  // variáveis que armazenam quais são os pinos do respectivo ultrassônico
 public:
-  void attach(int trigPin, int echoPin);
-
-  float read() {
-    digitalWrite(this->trigPin, LOW);
-    delayMicroseconds(2);
-    digitalWrite(this->trigPin, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(this->trigPin, LOW);
-    float distance = pulseIn(this->echoPin, HIGH) / 58.2;
-    return distance;
-  }
+  void attach(int trigPin, int echoPin);  // metodo que define os pinos comoo entrada e saída
+  float read();                           // método que retorna o valor do ultrasônico
 };
 
 void Ultrasonic::attach(int trigPin, int echoPin) {
@@ -25,42 +16,84 @@ void Ultrasonic::attach(int trigPin, int echoPin) {
   pinMode(this->echoPin = echoPin, INPUT);
 };
 
+float Ultrasonic::read() {
+  digitalWrite(this->trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(this->trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(this->trigPin, LOW);
+  float distance = pulseIn(this->echoPin, HIGH) / 58.2;
+  return distance;
+};
+
 
 class LED {
+private:
+  int pin;  // variável que armazena qual é o pino do respectivo LED
 public:
-  void attach(int pin);
+  void attach(int pin);                                  // metodo que define o pino comoo saída
+  void toggle(int state = true, bool isDigital = true);  // alterna o estado do LED, podendo ser de 0% a 100% quando analógico e desligado/ligado quando digital
 };
 
 void LED::attach(int pin) {
-  pinMode(pin, OUTPUT);
+  pinMode(this->pin = pin, OUTPUT);
 };
+
+void LED::toggle(int state, bool isDigital) {
+  if (isDigital)
+    digitalWrite(this->pin, state ? HIGH : LOW);
+  else
+    analogWrite(this->pin, map(state, 0, 100, 0, 255));
+
+  //Serial.println("LED[" + String(this->pin) + "] Turned on/off");
+  //Serial.println("LED[" + String(this->pin) + "] at " + String(map(state, 0, 100, 0, 255)) + "%");
+}
 
 
 class InfraRed {
+private:
+  int pin;  // variável que armazena qual é o pino do respectivo sensor infra vermelho
 public:
-  void attach(int pin, boolean isDigital);
+  void attach(int pin, boolean isDigital);  // metodo que define o pino comoo entrada caso seja digital
+  float getValue(bool isDigital);           // método que retorna o valor do sensor infra vermelho
 };
 
 void InfraRed::attach(int pin, boolean isDigital = true) {
   if (isDigital)
     pinMode(pin, INPUT);
+  this->pin = pin;
+};
+
+float InfraRed::getValue(bool isDigital) {
+  if (isDigital)
+    return digitalRead(this->pin);
+  else
+    return map(analogRead(this->pin), 0, 1023, 0, 100);
 };
 
 
-
 Servo servo[6];
+
+typedef enum {
+  _LEFT_ARM_SERVO,
+  _LEFT_CLAW_SERVO,
+  _RIGHT_CLAW_SERVO,
+  _CONTAINER_SERVO,
+} servos_index;
+
 Ultrasonic ultrasonic[6];
-//LED led[6];
-//InfraRed infraRed[3];
+LED led[6];
+InfraRed infraRed[3];
 
 void setup() {
-  Wire.begin(SLAVE_ADDRESS);    // define o endereço I2C e o inicia
+  Wire.begin(SLAVE_ADDRESS);    // define o endereço para comunicação I2C e o inicia
   Wire.onReceive(receiveData);  // irá chamar a função 'receiveData' ao receber informação através do I2C
   Wire.onRequest(sendData);     // após receber, irá requisitar que envie algo, e será chamado a função 'sendData'
 
   //servo[ID].attach(pin);
+  //servo[_LEFT_ARM_SERVO].attach(5);
   //ultrasonic[ID].attach(pin);
-  //led[ID].attach(pin);
+  //led[ID].attach(pin)
   //infraRed[ID].attach(pin);
 
   Serial.begin(9600);
@@ -101,41 +134,34 @@ void receiveData(int bytesIn) {
   for (int byte_count = 0; 1 < Wire.available(); byte_count++) {
     instruction[byte_count] = Wire.read();
   }
-  const byte dummyByte = Wire.read();                                           // lê o último byte fictício (não tem significado, mas precisa ser lido)
-  const boolean areDigital = instruction[_ID] < 100 || instruction[_ID] < 100;  // se alguns dos IDs forem maior que 100, significa que são analógicos
-  pendingValue = false;
+  const byte dummyByte = Wire.read();                // lê o último byte fictício (não tem significado, mas precisa ser lido)
+  const boolean isDigital = instruction[_ID] < 100;  // se o ID for maior que 100, significa que é analógico
+  pendingValue = false;                              // reseta a variável responsável por identificar se há dados a serem enviados
 
   if (instruction[_MAIN_ACTION] == 255) return;
 
   switch (instruction[_MAIN_ACTION]) {
     case _USE_LED:
-      if (areDigital) {
-        digitalWrite(instruction[_ID], instruction[_SECONDARY_ACTION] ? HIGH : LOW);
-        Serial.println("LED[" + String(instruction[_ID]) + "] Turned on/off");
-      } else {
-        instruction[_ID] -= 100;
-        analogWrite(instruction[_ID], map(instruction[_SECONDARY_ACTION], 0, 100, 0, 255));
-        Serial.println("LED[" + String(instruction[_ID]) + "] at " + String(map(instruction[_SECONDARY_ACTION], 0, 100, 0, 255)) + "%");
-      }
+      led[instruction[_ID]].toggle(isDigital, instruction[_SECONDARY_ACTION]);
       break;
 
     case _USE_SERVO:
       servo[instruction[_ID]].write(instruction[_SECONDARY_ACTION]);
-      Serial.println("Changed the angle of the Servo Motor[" + String(instruction[_ID]) + "] to " + String(instruction[_SECONDARY_ACTION]) + "degrees");
+      //Serial.println("Changed the angle of the Servo Motor[" + String(instruction[_ID]) + "] to " + String(instruction[_SECONDARY_ACTION]) + "degrees");
       break;
 
     case _USE_STEPPER_MOTOR:
 
       break;
     case _USE_INFRA_RED:
-      returnValue[0] = instruction[areDigital] ? digitalRead(instruction[_ID]) : map(analogRead(instruction[_ID]), 0, 1023, 0, 100);
-      Serial.println("Infra Red[" + String(instruction[_ID]) + "] = " + String(returnValue[0]));
+      returnValue[0] = infraRed[instruction[_ID]].getValue(isDigital);
+      //Serial.println("Infra Red[" + String(instruction[_ID]) + "] = " + String(returnValue[0]));
       pendingValue = true;
       break;
 
     case _USE_ULTRASONIC:
       returnValue[0] = ultrasonic[_ID].read();
-      Serial.println("Ultrasonic[" + String(instruction[_ID]) + "] = " + String(returnValue[0]));
+      //Serial.println("Ultrasonic[" + String(instruction[_ID]) + "] = " + String(returnValue[0]));
       pendingValue = true;
       break;
   }
