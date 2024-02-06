29574769246488508f3beca911c1af417f969713
alessandro sir <alessandrosiir@gmail.com>
2024-02-05 21:10:30 -0300

    17/09/2023 untested

--- I2C_ArduinoToEV3.ino
+++ I2C_ArduinoToEV3.ino
@@ -3,13 +3,14 @@
 
 #define SLAVE_ADDRESS 0x04  // endereço utlizado na comunicação I2C
 
-
 class Ultrasonic {
 private:
   int trigPin, echoPin;  // variáveis que armazenam quais são os pinos do respectivo ultrassônico
+  int timeOut = 5000;    // tempo máximo do pulso da medição. A partir de uma certa distância ocasiona um bug na comunicação I2C
 public:
-  void attach(int trigPin, int echoPin);  // metodo que define os pinos comoo entrada e saída
-  float read();                           // método que retorna o valor do ultrasônico
+  void attach(int trigPin, int echoPin);         // metodo que define os pinos como entrada e saída
+  float read();                                  // método que retorna o valor do ultrasônico
+  bool compare(int operatorNumber, int value2);  // método que lê o valor do ultrassônico e retorna o resultado da comparação com outro valor
 };
 
 void Ultrasonic::attach(int trigPin, int echoPin) {
@@ -23,48 +24,66 @@ float Ultrasonic::read() {
   digitalWrite(this->trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(this->trigPin, LOW);
-  float distance = pulseIn(this->echoPin, HIGH, 5000) / 58.2;
+  float distance = pulseIn(this->echoPin, HIGH, this->timeOut) / 58.2;
   return distance;
+};
+
+bool Ultrasonic::compare(int operatorNumber, int value2) {
+  int value1 = this->read();
+  switch (operatorNumber) {
+    case 0: return value1 == value2; break;
+    case 1: return value1 != value2; break;
+    case 2: return value1 > value2; break;
+    case 3: return value1 >= value2; break;
+    case 4: return value1 < value2; break;
+    case 5: return value1 <= value2; break;
+    default: return false;
+  }
 };
 
 
 class LED {
 private:
-  int pin;  // variável que armazena qual é o pino do respectivo LED
+  int pin;
+  bool isDigital;
 public:
-  void attach(int pin);                                  // metodo que define o pino comoo saída
-  void toggle(int state = true, bool isDigital = true);  // alterna o estado do LED, podendo ser de 0% a 100% quando analógico e desligado/ligado quando digital
+  void attach(int pin, bool isDigital);  // metodo que define o pino comoo saída
+  void toggle(int state = true);         // alterna o estado do LED
 };
 
-void LED::attach(int pin) {
-  pinMode(this->pin = pin, OUTPUT);
+void LED::attach(int pin, bool isDigital = true) {
+  if (isDigital) {
+    pinMode(this->pin = pin, OUTPUT);
+    this->isDigital = true;
+  }
 };
 
-void LED::toggle(int state, bool isDigital) {
-  if (isDigital)
+void LED::toggle(int state) {
+  if (this->isDigital)
     digitalWrite(this->pin, state ? HIGH : LOW);
   else
     analogWrite(this->pin, map(state, 0, 100, 0, 255));
-}
+};
 
 
 class InfraRed {
 private:
-  int pin;  // variável que armazena qual é o pino do respectivo sensor infra vermelho
-  boolean isDigital = false;
+  int pin;
+  bool isDigital;
 public:
-  void attach(int pin, bool isDigital);  // metodo que define o pino comoo entrada caso seja digital
-  float getValue();                      // método que retorna o valor do sensor infra vermelho
+  void attach(int pin, bool isDigital);  // metodo que define o pino como entrada caso seja digital
+  float read();                          // método que retorna o valor do sensor infra vermelho
 };
 
 void InfraRed::attach(int pin, bool isDigital = true) {
-  if (isDigital)
+  if (isDigital) {
     this->isDigital = true;
-  pinMode(pin, INPUT);
+    pinMode(pin, INPUT);
+  }
   this->pin = pin;
 };
 
-float InfraRed::getValue() {
+float InfraRed::read() {
   if (this->isDigital)
     return digitalRead(this->pin);
   else
@@ -72,7 +91,33 @@ float InfraRed::getValue() {
 };
 
 
-typedef enum {
+class Button {
+private:
+  int pin;
+  bool isDigital;
+public:
+  void attach(int pin, bool isDigital);  // metodo que define o pino como entrada caso seja digital
+  int read();                            // método que retorna o valor do botão
+};
+
+void Button::attach(int pin, bool isDigital = true) {
+  if (isDigital) {
+    pinMode(pin, INPUT);
+    this->isDigital = true;
+  }
+};
+
+int Button::read() {
+  if (this->isDigital) {
+    return digitalRead(this->pin);
+  } else {
+    return map(analogRead(this->pin), 0, 1023, 0, 100);
+  }
+};
+
+/* ----------------------------------------------------------------- */
+
+const typedef enum {
   _LEFT_ARM_SERVO,
   _RIGHT_ARM_SERVO,
   _LEFT_CLAW_SERVO,
@@ -81,39 +126,45 @@ typedef enum {
   _SERVO_LENGTH,
 } servos_index;
 
-typedef enum {
+const typedef enum {
   _FRONTAL_ULTRASONIC,
   _SIDE_ULTRASONIC,
   _ULTRASONIC_LENGTH,
 } ultrasonics_index;
 
-typedef enum {
+const typedef enum {
   _LED1,
   _LED2,
   _LED_LENGTH,
 } leds_index;
 
-typedef enum {
+const typedef enum {
   _DETECTOR_IR,
   _VERIFIER_IR,
   _IR_LENGTH,
 } infrareds_index;
 
+const typedef enum {
+  _BUMPER_BUTTON,
+  _BUTTON_LENGTH,
+} buttons_index;
+
 Servo servo[_SERVO_LENGTH];
 Ultrasonic ultrasonic[_ULTRASONIC_LENGTH];
 LED led[_LED_LENGTH];
 InfraRed infraRed[_IR_LENGTH];
+Button button[_BUTTON_LENGTH];
 
 
 void setup() {
   Wire.begin(SLAVE_ADDRESS);    // define o endereço para comunicação I2C e o inicia
-  Wire.onReceive(receiveData);  // irá chamar a função 'receiveData' ao receber informação através do I2C
-  Wire.onRequest(sendData);     // após receber, irá requisitar que envie algo, e será chamado a função 'sendData'
+  Wire.onReceive(receiveData);  // recebe dados através do I2C (a função 'receiveData' será executada toda vez que for utilizado o bloco 'Write')
+  Wire.onRequest(requestData);  // envia dados através do I2C (a função 'requestData' será executada toda vez que for utilizado o bloco 'Read')
 
-  servo[_LEFT_ARM_SERVO].attach(9);
+  servo[_LEFT_ARM_SERVO].attach(9, 0, 90);
   //servo[_RIGHT_ARM_SERVO].attach();
-  servo[_LEFT_CLAW_SERVO].attach(10);
-  servo[_RIGHT_CLAW_SERVO].attach(11);
+  servo[_LEFT_CLAW_SERVO].attach(10, 90 - 55, 90);
+  servo[_RIGHT_CLAW_SERVO].attach(11), 83, 83 + 55;
   //servo[_CONTAINER_SERVO].attach();
 
   ultrasonic[_FRONTAL_ULTRASONIC].attach(3, 4);
@@ -125,15 +176,12 @@ void setup() {
   infraRed[_DETECTOR_IR].attach(0, true);
   infraRed[_VERIFIER_IR].attach(1, false);
 
-  //pinMode(, INPUT); // BOTÃo
+  //button[_BUMPER_BUTTON].attach();
 
   Serial.begin(9600);
   while (!Serial) {
     delay(10);
   }
-
-  servo[_LEFT_CLAW_SERVO].write(90);
-  servo[_RIGHT_CLAW_SERVO].write(83);
 
   led[_LED1].toggle(true);
   Serial.println("Ready!");
@@ -145,125 +193,128 @@ void loop() {
 
 
 typedef enum {
-  _MAIN_ACTION,       // ação principal
-  _SECONDARY_ACTION,  // ação secundária
-  _ACTION_3,          // terceira ação
-  _ACTION_4,          // quarta ação
-  _ID,                // identificador
+  _MAIN_ACTION,  // ação principal
+  _ACTION_2,     // ação secundária
+  _ACTION_3,     // terceira ação
+  _VALUE_1,      // primeiro valor
+  _VALUE_2,      // segundo valor
+  _ID_1,         // primeiro identificador
+  _ID_2,         // segundo identificador
 } instruction_index;
 
-typedef enum {  // index das ações utilizado nas listas
+typedef enum {  // index das ações principais
   _USE_LED,
-  _USE_SERVO,
+  _USE_SERVO_DEGREE,
+  _USE_SERVO_PERCENTAGE,
+  _USE_SERVO_TO_MAX,
+  _USE_TWO_SERVOS_DEGREE,
+  _USE_TWO_SERVOS_PERCENTAGE,
+  _USE_TWO_SERVOS_TO_MAX,
   _USE_STEPPER_MOTOR,
   _USE_INFRA_RED,
   _USE_ULTRASONIC,
   _USE_COMPARE_ULTRASONIC,
-  _USE_BUTTOM,
-  _CHANGE_CLAW,
-  _CHANGE_CLAW_ARMS,
-  _CHANGE_CONTAINER_DOOR,
+  _USE_BUTTON,
 } options_index;
 
 
-uint8_t instruction[] = { 255, 0, 0, 0, 0, 0, 0, 0 };  // valores recebidor por I2C
-uint8_t returnValue[] = { 255, 0, 0, 0, 0, 0, 0, 0 };  // valores que serão retornados por I2C
-boolean pendingValue;                                  // se possui algum valor para ser enviado
+uint8_t instruction[] = { 0, 0, 0, 0, 0, 0, 0, 0 };  // valores recebidos por I2C
+uint8_t returnValue[] = { 0, 0, 0, 0, 0, 0, 0, 0 };  // valores que serão enviados por I2C
+boolean pendingValue;                                // se possui algum valor para ser enviado
+int fragmentIndex;                                   // variável utilizada para identificar os fragmentos
+boolean ignoreFirstSend;                             // usada para ignorar o envio automático
 
-int value;
-int b[4];
-int rest;
-int amountOfFragments = 4;
-int fragmentIndex = -1;
-boolean ignore;
 
 void receiveData(int bytesIn) {
-  for (int byte_count = 0; 1 < Wire.available(); byte_count++) {
+  for (int byte_count = 0; 1 < Wire.available(); byte_count++) {  // recebe os valores por I2C
     instruction[byte_count] = Wire.read();
   }
   const byte dummyByte = Wire.read();  // lê o último byte fictício (não tem significado, mas precisa ser lido)
-  const boolean isDigital = true;      //
   pendingValue = false;                // reseta a variável responsável por identificar se há dados a serem enviados
-
-  if (instruction[_MAIN_ACTION] == 255) return;
+  fragmentIndex = 0;                   // reseta a variável responsável por guardar a quantidade de fragmentos à serem retornados
 
   switch (instruction[_MAIN_ACTION]) {
     case _USE_LED:
-      led[instruction[_ID]].toggle(isDigital, instruction[_SECONDARY_ACTION]);
+      led[instruction[_ID_1]].toggle(instruction[_VALUE_1]);
       break;
 
-    case _USE_SERVO:
-      servo[instruction[_ID]].write(instruction[_SECONDARY_ACTION]);
-      //Serial.println("Changed the angle of the Servo Motor[" + String(instruction[_ID]) + "] to " + String(instruction[_SECONDARY_ACTION]) + "degrees");
+    case _USE_SERVO_DEGREE:
+      servo[instruction[_ID_1]].write(instruction[_VALUE_1]);
+      break;
+
+    case _USE_SERVO_PERCENTAGE:
+      servo[instruction[_ID_1]].write(map(instruction[_VALUE_1], 0, 100, 0, 180));
+      break;
+
+    case _USE_SERVO_TO_MAX:
+      servo[instruction[_ID_1]].write(instruction[_VALUE_1] ? 180 : 0);
+      break;
+
+    case _USE_TWO_SERVOS_DEGREE:
+      servo[instruction[_ID_1]].write(instruction[_VALUE_1]);
+      servo[instruction[_ID_2]].write(instruction[_VALUE_2]);
+      break;
+
+    case _USE_TWO_SERVOS_PERCENTAGE:
+      servo[instruction[_ID_1]].write(map(instruction[_VALUE_1], 0, 100, 0, 180));
+      servo[instruction[_ID_2]].write(map(instruction[_VALUE_2], 0, 100, 0, 180));
+      break;
+
+    case _USE_TWO_SERVOS_TO_MAX:
+      servo[instruction[_ID_1]].write(instruction[_VALUE_1] ? 180 : 0);
+      servo[instruction[_ID_2]].write(instruction[_VALUE_2] ? 180 : 0);
       break;
 
     case _USE_STEPPER_MOTOR:
 
       break;
+
     case _USE_INFRA_RED:
-      returnValue[0] = infraRed[instruction[_ID]].getValue();
-      //Serial.println("Infra Red[" + String(instruction[_ID]) + "] = " + String(returnValue[0]));
+      returnValue[0] = infraRed[instruction[_ID_1]].read();
       pendingValue = true;
       break;
 
     case _USE_ULTRASONIC:
-      value = constrain(round(ultrasonic[instruction[_ID]].read()*10)/10, 0, 60);
-      rest = 0;
+      int value = constrain(round(ultrasonic[instruction[_ID_1]].read() * 10) / 10, 0, 60);
 
-      for (int i = amountOfFragments - 1; i > 0; i--) {
-        rest = value % 7;
-        value = (value - rest) / 7;
-        b[i] = rest;
+      for (int v = value; (v /= 8) >= 1; fragmentIndex++) {}  // calcula quantos digitos o número convertido vai ter
+
+      for (int i = fragmentIndex; value; i--) {  // faz a conversão de base decimal para octal
+        returnValue[i] = value % 8;
+        value /= 8;
       }
-      b[0] = value;
 
-      fragmentIndex = amountOfFragments - 1;
+      fragmentIndex = instruction[_ACTION_2] - 1;  // quantidade de digitos que o usuário quer que retorne
 
       pendingValue = true;
       break;
 
     case _USE_COMPARE_ULTRASONIC:
-      value = ultrasonic[instruction[_ID]].read();
-      if (value <= instruction[_SECONDARY_ACTION]) {
-        returnValue[0] = 1;
-      } else {
-        returnValue[0] = 0;
-      }
+      returnValue[0] = ultrasonic[instruction[_ID_1]].compare(instruction[_ACTION_2], instruction[_VALUE_1]);
       pendingValue = true;
       break;
 
-    case _USE_BUTTOM:  // CRIAR CLASSE BOTÂO
-      returnValue[0] = digitalRead(instruction[_ID]);
+    case _USE_BUTTON:
+      returnValue[0] = button[instruction[_ID_1]].read();
       pendingValue = true;
-      break;
-
-    case _CHANGE_CLAW:
-      servo[_LEFT_CLAW_SERVO].write(instruction[_SECONDARY_ACTION] ? 90 - 55 : 90);
-      servo[_RIGHT_CLAW_SERVO].write(instruction[_ACTION_3] ? 83 + 55 : 83);
-      break;
-
-    case _CHANGE_CLAW_ARMS:
-      servo[_LEFT_ARM_SERVO].write(instruction[_SECONDARY_ACTION] ? 180 - 90 : 180);
-      servo[_RIGHT_ARM_SERVO].write(instruction[_ACTION_3] ? 0 + 90 : 0);
-      break;
-
-    case _CHANGE_CONTAINER_DOOR:
-      servo[_CONTAINER_SERVO].write(instruction[_SECONDARY_ACTION] * 90);
       break;
   }
-  ignore = true;
+
+  ignoreFirstSend = true;
 }
 
-void sendData() {
+void requestData() {
   if (pendingValue) {
-    if (ignore) {
-      ignore = false;
+    if (ignoreFirstSend) {
+      ignoreFirstSend = false;
       return;
     }
-    if (fragmentIndex >= 0) {
-      returnValue[0] = b[fragmentIndex--];
-    }
-    Serial.println("================");
-    Wire.write(constrain(returnValue[0], 0, 7));  // Envia o valor obtido pelo sensor
+
+    returnValue[fragmentIndex] = constrain(returnValue[fragmentIndex], 0, 7);  // evita que o valor seja maior que 7
+    Wire.write(returnValue[fragmentIndex]);                                    // envia o valor por I2C
+    returnValue[fragmentIndex] = 0;
+
+    if (fragmentIndex > 0)
+      fragmentIndex--;
   }
 }
